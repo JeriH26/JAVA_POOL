d251d3ce58ba71fb0ed4a98b011cea8ee0d3cbe7
Jeri_H <82069470+JeriH26@users.noreply.github.com>
2026-01-26 16:26:41 -0800

    Create a DeveloperProfile.java file

---
+++ DeveloperProfile.java
@@ -0,0 +1,31 @@
+// This project is developed by Junyi Huang, a student enrolled in the MSIS program at the Trine University.
+/*  Given that I have been hired as a junior software developer for TrineTech Solutions, a technology services 
+    firm that builds internal tools for client onboarding and data reporting.
+    The purpose of this project is to  to write and explain a simple Java program that introduces who you are-
+    similar to a developer profile
+*/
+
+// DeveloperProfile.java
+public class DeveloperProfile {
+    public static void main(String[] args) {
+
+        // Print developer name
+        System.out.println("Name: Junyi Huang");
+        System.out.println("\n"); // Adding a newline for better readability
+
+        // Print date enrolled
+        System.out.println("Date Enrolled: 23 Sep, 2024");
+        System.out.println("\n"); // Adding a newline for better readability
+
+        // Print major
+        System.out.println("Major: MSIS, Information Science");
+        System.out.println("\n"); // Adding a newline for better readability
+
+        // Print completed classes
+        System.out.println("Completed Class: Network Management");
+        System.out.println("\n"); // Adding a newline for better readability
+
+        // Print current class
+        System.out.println("Current Class: Object Oriented Programming in Java");
+    }
+}
